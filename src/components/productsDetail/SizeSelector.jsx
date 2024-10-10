d9c002dab946e196b9675b1277e6e9bc7e9bf09d
= <=>
2024-10-10 20:51:46 +0530

    Added Size Guide and Added links to the spotlight section products

--- src/components/productsDetail/SizeSelector.jsx
+++ src/components/productsDetail/SizeSelector.jsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import { HiOutlineArrowRight } from "react-icons/hi";
-export default function SizeSelector({ sizes, defaultSize, selectSize  }) {
-    
-  
-    return (
-      <div className="flex flex-col gap-4 tracking-tighter">
-        <h2 className="font-bold text-lg">
-          Select Size | <span className="underline"> Size Guide</span>{" "}
-          <HiOutlineArrowRight className="inline" />
-        </h2>
-       
-        <div className="flex gap-4">
-          {sizes.map(size => (
-            <SizeItem key={size.name} enabled={size.enabled} name={size.name} selectSize={selectSize} defaultSizeName={defaultSize} />
-          ))}
-  
-          {/* {sizes.disabledSizes?.map((size, index) => (
+export default function SizeSelector({ sizes, defaultSize, selectSize }) {
+  const [sizeGuideVisible, setSizeGuideVisible] = useState(false);
+
+  const toggleSizeGuideVisibility = ()=>{
+    setSizeGuideVisible(state=>!state);
+  }
+
+  return (
+    <div className="flex flex-col gap-4 tracking-tighter">
+      {/* Size Guide */}
+      {sizeGuideVisible && (
+        <div onClick={toggleSizeGuideVisibility} className="bg-black/50 fixed  top-0 right-0 bottom-0 left-0 z-[9900000] flex  bg-[rgb()] items-center justify-center">
+          <img
+            onClick={e=>e.stopPropagation()}
+            className="xl:h-full h-5/6"
+            src="/productDetail/sizeGuide.jpeg"
+            alt=""
+          />
+        </div>
+      )}
+      <h2 className="font-bold text-lg">
+        Select Size | <button className="underline" onClick={toggleSizeGuideVisibility}> Size Guide</button>{" "}
+        <HiOutlineArrowRight className="inline" />
+      </h2>
+
+      <div className="flex gap-4">
+        {sizes.map((size) => (
+          <SizeItem
+            key={size.name}
+            enabled={size.enabled}
+            name={size.name}
+            selectSize={selectSize}
+            defaultSizeName={defaultSize}
+          />
+        ))}
+
+        {/* {sizes.disabledSizes?.map((size, index) => (
             <button
               key={index}
               className={`border-2 w-8 h-8 border-[#BEBCBD] disabled:text-gray-300 `}
@@ -23,28 +45,26 @@ export default function SizeSelector({ sizes, defaultSize, selectSize  }) {
               {size}
             </button>
           ))} */}
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  function SizeItem({ name, defaultSizeName, selectSize, enabled }) {
-    const clickHandler = () => {
-      
-      selectSize("Size", name)
-    };
-  
-    return (
-      <button
-        title={enabled ? name : "Not available with other selected options!"}
-        onClick={clickHandler}
-        className={`border-2 w-8 h-8 border-[#BEBCBD] disabled:opacity-25   ${
-          name === defaultSizeName ? "bg-[#1F4A40] text-white" : ""
-        }`}
-        disabled={!enabled}
-      >
-        {name}
-      </button>
-    );
-  }
-  
+function SizeItem({ name, defaultSizeName, selectSize, enabled }) {
+  const clickHandler = () => {
+    selectSize("Size", name);
+  };
+
+  return (
+    <button
+      title={enabled ? name : "Not available with other selected options!"}
+      onClick={clickHandler}
+      className={`border-2 w-8 h-8 border-[#BEBCBD] disabled:opacity-25   ${
+        name === defaultSizeName ? "bg-[#1F4A40] text-white" : ""
+      }`}
+      disabled={!enabled}
+    >
+      {name}
+    </button>
+  );
+}
